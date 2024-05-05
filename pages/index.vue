71cab6c370160a9f98c98a6b32bf368ae15bbad5
Albert <albert.tumba@gmail.com>
2024-05-05 23:30:13 +0200

    installation sass & tailwindCSS

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
+<script setup>
+useSeoMeta({
+  title: "albertcode.com | Développeur freelance",
+  description: "Développeur freelance on Vue JS",
+});
+</script>
+
 <template>
   <div class="Main">
     <h1>Développeur Freelance</h1>
+    
   </div>
 </template>
