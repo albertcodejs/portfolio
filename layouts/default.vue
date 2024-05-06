50ce1f8dfa2763d56437347ed622f09d606e4cd1
Albert <albert.tumba@gmail.com>
2024-05-06 14:53:47 +0200

    ajout des logo de langages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="navbar">
-    <nav>
-      <a class="text-red-800">Projets</a>
+    <nav class="flex items-center justify-center py-2.5">
+      <a>Projets</a>
       <a href="/">
-        <img src="/public/img/Logo_AT.svg" width="62.5" height="62.5" />
+        <img src="/public/img/logo_AT.svg" width="50" class="mx-14" />
       </a>
       <a>Contact</a>
     </nav>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
 <script setup>
 useSeoMeta({
-  title: "albertcode.com | Développeur freelance",
+  title: "albertcode.com | Développeur Front End",
   description: "Développeur freelance on Vue JS",
 });
 </script>
 
 <template>
   <div class="Main">
-    <h1>Développeur Freelance</h1>
-    
+    <div class="flex flex-col items-center justify-center">
+      <div class="flex flex-col items-center justify-center py-20">
+        <img src="/public/img/memoji-apple.jpg" width="300" />
+        <h1 class="uppercase">DÉVELOPPEUR</h1>
+        <h1 class="uppercase">FRONT END</h1>
+      </div>
+      <div class="flex flex-col items-center">
+        <p class="text-center">
+          "If you have everything under control, you are not moving fast
+          enough."
+        </p>
+        <p>Mario Andrettict</p>
+      </div>
+      <div></div>
+    </div>
+    <div></div>
   </div>
 </template>
